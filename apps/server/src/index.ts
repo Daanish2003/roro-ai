5bd2d9e41a65759e7bdfd2d3bef4d1fb4ed36487
Daanish2003 <redchillies6786@gmail.com>
2025-04-12 18:34:43 +0000

    fix:port

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,8 +6,14 @@ const port = process.env.PORT || 4000;
 
 const server = createServer(app)
 
-server.listen(port, () => {
+
+server.listen({
+  host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+  port: port
+}, () => {
   console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://0.0.0.0:${port}`);
+  console.log(process.env.FRONTEND_URL)
 });
 
 server.on('error', console.error);
