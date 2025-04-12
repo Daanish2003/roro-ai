3ac82b4b8499225521e77103f2701d65a3f0a17a
Daanish2003 <redchillies6786@gmail.com>
2025-04-12 15:14:00 +0000

    fix:cors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     exposedHeaders: [
         'set-auth-token',
