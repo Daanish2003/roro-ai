5eb71e15c637b039726ea362cdde7e97b9899ffb
Daanish <redchillies6786@gmail.com>
2025-04-13 14:44:32 +0530

    Revert "fix-redis-retry"
    This reverts commit 186d990f57af59daa809cf94ff47c39148759073.

--- packages/database/src/cache.ts
+++ packages/database/src/cache.ts
@@ -20,11 +20,6 @@ class RedisClient {
       password,
       host: redisUrl,
       port: Number(port),
-      tls: {},
-      retryStrategy(times) {
-        const delay = Math.min(times * 100, 3000);
-        return delay;
-      },
     });
     this.setupEventHandlers();
   }
