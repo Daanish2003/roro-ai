75448ab6c47f23a083efdbf2175f4aa7e700c59a
Daanish2003 <redchillies6786@gmail.com>
2025-04-14 00:22:20 +0000

    fix:jwks

--- apps/media/src/module/jwt.ts
+++ apps/media/src/module/jwt.ts
@@ -2,11 +2,11 @@ import { jwtVerify, createRemoteJWKSet } from 'jose'
  
 export async function validateToken(token: string) {
     const JWKS = createRemoteJWKSet(
-      new URL('http://localhost:4000/api/auth/jwks')
+      new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/jwks`)
     )
     const { payload } = await jwtVerify(token, JWKS, {
-      issuer: 'http://localhost:4000', // Should match your JWT issuer which is the BASE_URL
-      audience: 'http://localhost:5000', // Should match your JWT audience which is the BASE_URL by default
+      issuer: process.env.NEXT_PUBLIC_BACKEND_URL,
+      audience: process.env.NEXT_PUBLIC_MEDIA_URL,
     })
     return payload
 }
