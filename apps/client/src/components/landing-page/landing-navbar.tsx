39b76c76f07a917bfb42aff891370fb1fd38a5ec
Daanish2003 <redchillies6786@gmail.com>
2025-04-16 12:02:58 +0000

    feat:dark-mode

--- apps/client/src/components/landing-page/landing-navbar.tsx
+++ apps/client/src/components/landing-page/landing-navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Logo from './logo'
 import LoginButton from './login-button'
 import GithubStarButton from './github-start-button'
-import { ToggleThemeButton } from './toggle-theme-button'
 import { Separator } from '@roro-ai/ui/components/ui/separator'
 
 export default function LandingNavbar() {
@@ -10,7 +9,6 @@ export default function LandingNavbar() {
     <div className='border-b px-6 py-2 flex justify-between items-center'>
         <Logo />
         <div className='flex items-center gap-x-2'>
-        <ToggleThemeButton />
         <GithubStarButton />
         <Separator orientation='vertical' className='h-8'/>
         <LoginButton />
