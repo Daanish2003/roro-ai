7a46bb706e868d6f0abbf067d24ed2445904405d
Daanish2003 <redchillies6786@gmail.com>
2025-04-19 14:02:13 +0000

    fix-server-logs

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,10 @@
 import { createServer } from "node:http";
 import "dotenv/config"
 import { app } from "./api/index.js"
-import { redisSub } from "./api/utils/redis.js";
 
 const port = process.env.PORT || 4000;
 
 const server = createServer(app)
-
-redisSub.connect()
-redisSub.subscribe('CHAT_CLOSED')
-
 
 server.listen(port ,() => {
   console.log(`Listening at http://localhost:${port}`);
